35ffd903d457a6c2cf0fa7d578855e91a0e1a4b0
Sahil Hadke <sahil.hadke@gmail.com>
2024-11-07 12:31:39 -0700

    yelp working

--- yelp.py
+++ yelp.py
@@ -1,67 +1,119 @@
-"""A complete end-to-end test for an e-commerce website with UC mode enabled."""
+"""A complete end-to-end test for an e-commerce website."""
 from seleniumbase import BaseCase, SB
+BaseCase.main(__name__, __file__)
 import random
 import json
 
+
 class MyTestClass(BaseCase):
+
     def test_swag_labs(self):
-        # Enable UC mode by setting `uc=True`
-        with SB(browser="firefox", uc=True) as sb:
-            sb.maximize_window()
-            name = "Thai Basil"
-            location = "Tempe"
-            name = name.replace(" ", "+").lower().strip()
-            location = location.replace(" ", "+").lower().strip()
+        
+        self.maximize_window()
+        
+        name = "Tha Dhaba"
+        location = "tempe"
+        name = name.replace(" ", "+").lower().strip()
+        location = location.replace(" ", "+").lower().strip()
+       
+        self.open(f"https://www.google.com/search?q={name}+{location}+yelp+reviews")
 
-            sb.open(f"https://www.google.com/search?q={name}+{location}+yelp+reviews")
-            sb.wait(random.randint(2, 5))
+        self.wait(random.randint(3, 6))
 
-            # Click on the first link
-            sb.click("(//*[contains(@class, 'LC20lb')])[1]")
+        # click on first google link
+        if self.is_element_visible("(//*[contains(@class, 'LC20lb')])[1]"):
+            self.click("(//*[contains(@class, 'LC20lb')])[1]")
+        else:
+            print('Wrong google search, try different name')
+            return
+        
+        self.wait(random.randint(3, 6))
 
-            sb.wait(10)
-            sb.scroll_into_view("#reviews")
+        # get current url
+        url = self.get_current_url()
+        url = url + '#reviews'
+        self.open(url)
 
-            reviews = []
+        self.wait(random.randint(5, 10))
+        reviews = []
 
-            count = 1
-            while count < 5:
-                # Scroll to review count
-                if sb.is_element_visible(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div/div/div/div/div/div[2]/div/span"):
-                    sb.scroll_into_view(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div/div/div/div/div/div[2]/div/span")
-                    sb.wait(random.randint(2, 5))
+        count = 1
+        current_page = 1
+        while count < 15:
 
-                    # Get name of the reviewer
-                    reviewer_name = sb.get_text(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div/div/div/div/div/div[2]/div/span")
-                    sb.wait(random.randint(2, 5))
+            name = ""
+            star = ""
+            date = ""
+            review = ""
 
-                    # Get star rating
-                    star = sb.get_attribute(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div[2]/div/div/div/div/div", 'aria-label')
-                    sb.wait(random.randint(2, 5))
-
-                    # Get date
-                    date = sb.get_text(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div[2]/div/div[2]/span")
-                    sb.wait(random.randint(2, 5))
-
-                    # Get review text
-                    review = sb.get_text(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div[4]/p")
-                    sb.wait(random.randint(2, 5))
-                    count += 1
-
-                    temp_dict = {
-                        "name": reviewer_name,
-                        "star": star,
-                        "date": date,
-                        "review": review
-                    }
-
-                    reviews.append(temp_dict)
+            
+            if current_page >= 10:
+                # press next button
+                next_button_xpath = f"//div[@id='reviews']//ul[starts-with(@class, ' list__')]/../div[starts-with(@class, 'pagination__')]//a[starts-with(@class, 'next-link')]"
+                if self.is_element_visible(next_button_xpath):
+                    self.click(next_button_xpath)
+                    self.wait(random.randint(3, 6))
+                    current_page = 1
                 else:
-                    # Click on next
-                    sb.click("//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../div[5]/div/div/*/*/*/*[contains(@class, 'icon--24-chevron-right-v2')]")
+                    print('No next button found')
+                    break
 
-            with open("reviews_yelp.json", "w", encoding="utf-8") as file:
-                json.dump(reviews, file, ensure_ascii=False, indent=4)
 
-# Run the test case
-BaseCase.main(__name__, __file__)
+            # scroll to review count
+            review_count_xpath = f"//div[@id='reviews']//ul[starts-with(@class, ' list__')]/li[{current_page}]"
+            if self.is_element_visible(review_count_xpath):
+                self.scroll_into_view(review_count_xpath)
+                self.wait(random.randint(3, 6))
+            else:
+                print('No review count found')
+                break
+
+            # get name
+            name_xpath = f"(//div[@id='reviews']//ul[starts-with(@class, ' list__')]/li[{current_page}]//a[starts-with(@href, '/user_details')])[2]"
+            if self.is_element_visible(name_xpath):
+                name = self.get_text(name_xpath)
+            else:
+                print('No name found')
+                break
+
+            # get star
+            star_xpath = f"//div[@id='reviews']//ul[starts-with(@class, ' list__')]/li[{current_page}]//div[starts-with(@role, 'img')]"
+            if self.is_element_visible(star_xpath):
+                star = self.get_attribute(star_xpath, "aria-label")
+                
+            else:
+                print('No star found')
+                break
+
+            # get date
+            date_xpath = f"//div[@id='reviews']//ul[starts-with(@class, ' list__')]/li[{current_page}]//div[starts-with(@role, 'img')]/../../../../div[2]/span"
+            if self.is_element_visible(date_xpath):
+                date = self.get_text(date_xpath)
+            else:
+                print('No date found')
+                break
+
+            # get review
+            review_xpath = f"//div[@id='reviews']//ul[starts-with(@class, ' list__')]/li[{current_page}]//p[starts-with(@class, 'comment_')]/span"
+            if self.is_element_visible(review_xpath):
+                review = self.get_text(review_xpath)
+            else:
+                print('No review found')
+                break
+
+            self.wait(random.randint(3, 6))
+
+            reviews.append({
+                "name": name,
+                "star": star,
+                "date": date,
+                "review": review
+            })
+
+            count += 1
+            current_page += 1   
+
+        print(reviews)
+
+        with open("reviews_yelp.json", "w", encoding="utf-8") as file:
+            json.dump(reviews, file, ensure_ascii=False, indent=4)
--- yelp2.py
+++ yelp2.py
@@ -1,65 +1,72 @@
-from seleniumbase import Driver
-import random 
-import time
+"""A complete end-to-end test for an e-commerce website with UC mode enabled."""
+from seleniumbase import BaseCase, SB
+import random
+import json
 
-name = "Thai Basil"
-location = "Tempe"
+class MyTestClass(BaseCase):
+    def test_swag_labs(self):
+        # Enable UC mode by setting `uc=True`
+        with SB(browser="firefox", uc=True) as sb:
+            sb.maximize_window()
+            name = "Thai Basil"
+            location = "Tempe"
+            name = name.replace(" ", "+").lower().strip()
+            location = location.replace(" ", "+").lower().strip()
 
-name = name.replace(" ", "+").lower().strip()
-location = location.replace(" ", "+").lower().strip()
+            sb.open(f"https://www.google.com/search?q={name}+{location}+yelp+reviews")
+            sb.wait(random.randint(2, 5))
 
-driver = Driver(browser='firefox', uc=True)
-driver.get(f"https://www.google.com/search?q={name}+{location}+yelp+reviews")
+            # Click on the first link
+            sb.click("(//*[contains(@class, 'LC20lb')])[1]")
 
-time.sleep(random.randint(2, 5))
+            sb.wait(random.randint(6, 10))
+            # get current url
+            url = sb.get_current_url()
+            url = url + '#reviews'
 
-driver.click("(//*[contains(@class, 'LC20lb')])[1]")
-time.sleep(random.randint(7,10))
+            sb.open(url)
+            sb.wait(random.randint(1000, 2000))
 
+            reviews = []
 
-driver.scroll_to("#reviews")
+            count = 1
+            while count < 5:
+                # Scroll to review count
+                if sb.is_element_visible(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div/div/div/div/div/div[2]/div/span"):
+                    sb.scroll_into_view(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div/div/div/div/div/div[2]/div/span")
+                    sb.wait(random.randint(2, 5))
 
-reviews = []
+                    # Get name of the reviewer
+                    reviewer_name = sb.get_text(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div/div/div/div/div/div[2]/div/span")
+                    sb.wait(random.randint(2, 5))
 
-count = 1
-while count < 5:
+                    # Get star rating
+                    star = sb.get_attribute(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div[2]/div/div/div/div/div", 'aria-label')
+                    sb.wait(random.randint(2, 5))
 
-    # scroll to review count
-    if driver.is_element_visible(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div/div/div/div/div/div[2]/div/span"):
-        driver.scroll_to(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div/div/div/div/div/div[2]/div/span")
-        time.sleep(random.randint(2, 5))
+                    # Get date
+                    date = sb.get_text(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div[2]/div/div[2]/span")
+                    sb.wait(random.randint(2, 5))
 
-        # get name of the reviewer
-        name = driver.get_text(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div/div/div/div/div/div[2]/div/span")
-        time.sleep(random.randint(2, 5))
+                    # Get review text
+                    review = sb.get_text(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div[4]/p")
+                    sb.wait(random.randint(2, 5))
+                    count += 1
 
-        # get review
-        review = driver.get_text(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div/div/div/div/div/div[2]/div[2]/div[2]/span")
-        time.sleep(random.randint(2, 5))
+                    temp_dict = {
+                        "name": reviewer_name,
+                        "star": star,
+                        "date": date,
+                        "review": review
+                    }
 
-        # get rating    
-        rating = driver.get_text(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div/div/div/div/div/div[2]/div[2]/div[1]/div/span")
-        time.sleep(random.randint(2, 5))
+                    reviews.append(temp_dict)
+                else:
+                    # Click on next
+                    sb.click("//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../div[5]/div/div/*/*/*/*[contains(@class, 'icon--24-chevron-right-v2')]")
 
-        # get date
-        date = driver.get_text(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div/div/div/div/div/div[2]/div[2]/div[3]/span")
-        time.sleep(random.randint(2, 5))
+            with open("reviews_yelp.json", "w", encoding="utf-8") as file:
+                json.dump(reviews, file, ensure_ascii=False, indent=4)
 
-        # get time
-        time = driver.get_text(f"//*[contains(@aria-labelledby, 'inline-search-:rs:')]/../../../../../../../../ul/li[{count}]/div/div/div/div/div/div/div[2]/div[2]/div[4]/span")
-        time.sleep(random.randint(2, 5))
-
-        reviews.append({
-            "name": name,
-            "review": review,
-            "rating": rating,
-            "date": date,
-            "time": time
-        })
-        count += 1
-    else:
-        break
-print(reviews)
-
-
-
+# Run the test case
+BaseCase.main(__name__, __file__)
